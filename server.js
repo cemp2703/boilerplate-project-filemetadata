0dce5e8a48a0ffd673ac1454f8c10744ffde8dee
César Palma Morante <cpalmamorante@gmail.com>
2020-10-05 13:14:23 +0000

    Ejercicio culminado

--- server.js
+++ server.js
@@ -2,9 +2,10 @@
 
 var express = require('express');
 var cors = require('cors');
-
 // require and use "multer"...
+var multer = require('multer')
 
+var upload = multer({ dest: './public/' })
 var app = express();
 
 app.use(cors());
@@ -13,6 +14,10 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.get('/', function (req, res) {
      res.sendFile(process.cwd() + '/views/index.html');
   });
+
+app.post('/api/fileanalyse', upload.single('upfile'),(req,res)=>{
+  res.json({name:req.file.originalname,type:req.file.mimetype,size:req.file.size})
+})
 
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
